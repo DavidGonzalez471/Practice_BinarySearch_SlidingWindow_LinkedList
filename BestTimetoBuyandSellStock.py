decc4eb20060ddba288b56d2061270c352622e73
DavidGonzalez471 <88919781+DavidGonzalez471@users.noreply.github.com>
2023-02-22 18:29:59 -0500

    Create BestTimetoBuyandSellStock.py

---
+++ BestTimetoBuyandSellStock.py
@@ -0,0 +1,12 @@
+#check if lowest price is greater than zero at any point
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        lowest = prices[0]
+
+        for p in prices:
+            if p < lowest:
+                lowest = p
+            res = max(res, p - lowest)
+        return res
+            
