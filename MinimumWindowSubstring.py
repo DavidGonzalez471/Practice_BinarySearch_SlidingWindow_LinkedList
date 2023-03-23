ed74ff952e3cc5fabf3a5f093f340398095376dd
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-03-23 00:37:16 -0400

    Update MinimumWindowSubstring.py

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -1,1 +1,21 @@
+#
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s) or t == "":
+            return ""
+        
+        tCount, window = {}, {}
+
+        for c in t:
+            tCount[c] = 1 + tCount.get(c, 0)
+        
+        have, need = 0, len(tCount)
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            if window[c] == tCount
+
+        
+        
 
