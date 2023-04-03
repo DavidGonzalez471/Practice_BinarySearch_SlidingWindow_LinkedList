6b496ff77bca8e26e40d2d5aaaedb6ad93170fc0
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-04-03 12:44:06 -0400

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,1 +1,16 @@
-#
+#Reversing the linked list iteratively until we get to the end and get a null value then iterating backwards through the list.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
