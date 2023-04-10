c7271eaa97b48ebccfb030439ab5fcb7ceda2d5a
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-04-09 23:57:27 -0400

    Update ReorderList.py

--- ReorderList.py
+++ ReorderList.py
@@ -1,4 +1,4 @@
-#Solution finds the halfway point of the linked list and splits the list in two and traverses the second list backwards combining the two lists.
+#Solution finds the halfway point of the linked list and splits the list in two and traverses the right side backwards combining the two lists.
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,6 +6,16 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        right = slow.next
+        prev = slow.next = None
+        while right:
+            temp = right.next
+            right.next = prev
+            prev = right
+            right = temp
