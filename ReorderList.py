c60ab5bad37c1f209eb35f7684402bccedfa5806
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-04-10 17:07:39 -0400

    Update ReorderList.py

--- ReorderList.py
+++ ReorderList.py
@@ -19,3 +19,12 @@ class Solution:
             right.next = prev
             prev = right
             right = temp
+            print(prev)
+            print(right)
+        
+        left,right = head, prev
+        while right:
+            temp1, temp2 = left.next, right.next
+            left.next = right
+            right.next = temp1
+            left, right = temp1, temp2
