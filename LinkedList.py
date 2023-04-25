e49a5fa9756e986b85682d1dddb02838d73e9c6b
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-04-24 23:51:12 -0400

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,8 +1,17 @@
 #Implementation of Linked List, starting with singly Linked List.
+
 # Node class
 class Node:
-
 	# Function to initialize the node object
 	def __init__(self, data):
-		self.data = data
-		self.next = None # Initialize next as null
+		self.data = data # Assign data
+		self.next = None # Initialize
+		# next as null
+
+# Linked List class
+class LinkedList:
+	# Function to initialize the Linked
+	# List object
+	def __init__(self):
+		self.head = None
+
